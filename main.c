22b381ba386c76b7b54c25114bd3bcc0892f4a4a
tamitakada <tamitakada@gmail.com>
2021-10-22 15:23:16 -0400

    added create library func

--- main.c
+++ main.c
@@ -1,5 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "test.h"
+#include "linked_list.h"
+
+struct song_node ** create_library() {
+    struct song_node **lib = malloc(27 * sizeof(struct song_node));
+    
+    int i;
+    for (i = 0; i < 27; i++) {
+        lib[i] = 0;
+    }
+    
+    return lib;
+}
 
 int main() {
+    struct song_node **lib = create_library();
+    
     insert_front_tests();
 }
