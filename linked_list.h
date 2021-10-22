82b35b959b6b12bb33d802ff8c4ca46b19f0f45f
tamitakada <tamitakada@gmail.com>
2021-10-22 18:00:34 -0400

    added random

--- linked_list.h
+++ linked_list.h
@@ -14,5 +14,6 @@ struct song_node * insert(struct song_node *s, char *name, char *artist);
 int songcmp(struct song_node *first, struct song_node *second);
 struct song_node * free_list(struct song_node *s);
 struct song_node * remove_node(struct song_node *front, char *name, char *artist);
+struct song_node * get_random(struct song_node *s);
 
 #endif
